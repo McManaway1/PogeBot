8afa4d4509055c4948b38d1d9f15ac0a447e7488
McManaway1 <andrew.mcmanaway@gmail.com>
2018-01-18 15:34:17 +1300

    Simplified the Creation of CommandData Segments

--- src/main/java/com/teambeez/parsers/containers/CommandData.java
+++ src/main/java/com/teambeez/parsers/containers/CommandData.java
@@ -5,8 +5,11 @@ import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import org.apache.commons.collections4.list.UnmodifiableList;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,12 +18,31 @@ import java.util.List;
  */
 public class CommandData {
     private final String command, arguments;
-    private final MessageReceivedEvent event;
+    private final Guild guild;
+    private final Member member;
+    private final TextChannel channel;
+    private final List<User> mentions;
 
     public CommandData(String command, String arguments, MessageReceivedEvent event) {
         this.command = command;
         this.arguments = arguments;
-        this.event = event;
+        this.guild = event.getGuild();
+        this.member = event.getMember();
+        this.channel = event.getTextChannel();
+        this.mentions = event.getMessage().getMentionedUsers();
+    }
+
+    public CommandData(String command, String arguments, Guild guild, Member member, TextChannel channel) {
+        this(command, arguments, guild, member, channel, new ArrayList<>());
+    }
+
+    public CommandData(String command, String arguments, Guild guild, Member member, TextChannel channel, List<User> mentions) {
+        this.command = command;
+        this.arguments = arguments;
+        this.guild = guild;
+        this.member = member;
+        this.channel = channel;
+        this.mentions = mentions;
     }
 
     /**
@@ -49,21 +71,21 @@ public class CommandData {
      * @return The Guild the command was sent from.
      */
     public Guild getGuild() {
-        return event.getGuild();
+        return guild;
     }
 
     /**
      * @return The Member which sent the Command.
      */
     public Member getMember() {
-        return event.getMember();
+        return member;
     }
 
     /**
      * @return The TextChannel the command was sent from.
      */
     public TextChannel getChannel() {
-        return event.getMessage().getTextChannel();
+        return channel;
     }
 
     /**
@@ -71,8 +93,14 @@ public class CommandData {
      */
     @Nullable
     public User getFirstMention() {
-        List<User> mentions = event.getMessage().getMentionedUsers();
         if (mentions.isEmpty()) return null;
         return mentions.get(0);
+    }
+
+    /**
+     * @return An unmodifiable List of all mentioned Users in a Command
+     */
+    public List<User> getMentions() {
+        return Collections.unmodifiableList(new ArrayList<>(mentions));
     }
 }
