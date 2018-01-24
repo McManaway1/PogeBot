d29ecd18047ec03cf95397aa62f3ea69bbaaf011
McManaway1 <andrew.mcmanaway@gmail.com>
2018-01-24 13:21:30 +1300

    Added Example Pack

---
+++ src/test/java/com/example/ExamplePack.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import com.teambeez.parsers.containers.CommandData;
+import com.teambeez.packs.pack.IPack;
+import net.dv8tion.jda.core.events.Event;
+
+public class ExamplePack implements IPack{
+
+    @Override
+    public void initialize() {
+        System.out.println("Start!");
+    }
+
+    @Override
+    public void invoke(CommandData data) {
+        System.out.println("Invoked!");
+    }
+
+    @Override
+    public void onEvent(Event event) {
+        System.out.println("Event Occurred!");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Closed!");
+    }
+}
